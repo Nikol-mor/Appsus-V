3e150a421ee681a6f9b4afb4715d421d65d28e07
Ram Beer <ramron123@gmail.com>
2022-01-01 20:33:52 +0200

    shabat shalom

--- js/apps/mail/pages/MailMain.jsx
+++ js/apps/mail/pages/MailMain.jsx
@@ -1,10 +1,11 @@
 const { Route, Switch } = ReactRouterDOM;
 
-import { MailList } from '../cmps/MailList.jsx';
-import { MailNav } from '../cmps/MailNav.jsx';
-import { ComposeMail } from './ComposeMail.jsx';
-import { MailFilter } from '../cmps/MailFilter.jsx';
-import { MailDetails } from './MailDetails.jsx';
+import { MailList } from "../cmps/MailList.jsx";
+import { MailNav } from "../cmps/MailNav.jsx";
+import { ComposeMail } from "./ComposeMail.jsx";
+import { MailFilter } from "../cmps/MailFilter.jsx";
+import { MailDetails } from "./MailDetails.jsx";
+import { AppHeader } from "../../../cmps/AppHeader.jsx";
 // mail details
 
 export class MailMain extends React.Component {
@@ -13,12 +14,12 @@ export class MailMain extends React.Component {
   };
 
   onToggleComposeModal = () => {
-    console.log('state modal', this.state.isComposeModalShown);
+    console.log("state modal", this.state.isComposeModalShown);
     this.setState({ isComposeModalShown: !this.state.isComposeModalShown });
   };
 
   expandMail = (mailId) => {
-    this.props.history.push(this.props.location.pathname + '/' + mailId);
+    this.props.history.push(this.props.location.pathname + "/" + mailId);
   };
 
   replyMail = (mailId) => {
@@ -30,25 +31,28 @@ export class MailMain extends React.Component {
     const { mailId } = this.props.match.params;
 
     return (
-      <section className='mail-main'>
-        <MailFilter />
-        <MailNav onToggleComposeModal={this.onToggleComposeModal} />
-        {/* <Route component={MailDetails} path='/mail/:mailId' /> */}
-        {/* <Route component={MailList} path='/mail' /> */}
-        {this.state.isComposeModalShown && (
-          <ComposeMail onToggleComposeModal={this.onToggleComposeModal} />
-        )}
+      <div>
+        <AppHeader />
+        <section className="mail-main">
+          <MailFilter />
+          <MailNav onToggleComposeModal={this.onToggleComposeModal} />
+          {/* <Route component={MailDetails} path='/mail/:mailId' /> */}
+          {/* <Route component={MailList} path='/mail' /> */}
+          {this.state.isComposeModalShown && (
+            <ComposeMail onToggleComposeModal={this.onToggleComposeModal} />
+          )}
 
-        {!mailId ? (
-          <MailList
-            onToggleComposeModal={this.onToggleComposeModal}
-            expandMail={this.expandMail}
-            replyMail={this.replyMail}
-          />
-        ) : (
-          <MailDetails mailId={mailId} />
-        )}
-      </section>
+          {!mailId ? (
+            <MailList
+              onToggleComposeModal={this.onToggleComposeModal}
+              expandMail={this.expandMail}
+              replyMail={this.replyMail}
+            />
+          ) : (
+            <MailDetails mailId={mailId} />
+          )}
+        </section>
+      </div>
     );
   }
 }
